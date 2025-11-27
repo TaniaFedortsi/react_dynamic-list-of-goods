51ec4993dcde8a53c389c773a4c640547df44e27
TaniaFedortsi <tania.fdrts@gmail.com>
2025-11-27 14:57:41 +0200

    Add error handling for API requests

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,17 +4,33 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      return response.json();
+    })
+    .catch(() => {
+      return [];
+    });
 }
 
 export const get5First = () => {
-  return getAll().then(goods =>
-    [...goods].sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
-  ); // sort and get the first 5
+  return getAll()
+    .then(goods =>
+      [...goods].sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
+    )
+    .catch(() => {
+      return [];
+    });
 };
 
 export const getRedGoods = () => {
-  return getAll().then(goods =>
-    [...goods].filter(good => good.color === 'red'),
-  ); // get only red
+  return getAll()
+    .then(goods => [...goods].filter(good => good.color === 'red'))
+    .catch(() => {
+      return [];
+    });
 };
